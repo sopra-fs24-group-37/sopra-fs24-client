e15dc17364817edc4b3372dba1b45d9c836aeb01
Gian-Luca <gian-luca.fuehrer@uzh.ch>
2024-04-16 09:49:46 +0200

    fix: adjusted indentation

--- src/components/views/GameRound.tsx
+++ src/components/views/GameRound.tsx
@@ -9,35 +9,41 @@ import SwissMap from "components/ui/SwissMap";
 import "leaflet/dist/leaflet.css";
 import Timer from "components/ui/Timer";
 
-
 const GameRound = () => {
   const [players, setPlayers] = useState<User[]>([]);
   const navigate = useNavigate();
   const [imageUrl, setImageUrl] = useState("");
   const [location, setLocation] = useState({ lat: 46.8182, lng: 8.2275 });
-  const [timer, setTimer] = useState(10); 
+  const [timer, setTimer] = useState(10);
 
   useEffect(() => {
     const fetchImage = async () => {
       try {
-        const response = await axios.get("https://api.unsplash.com/photos/random", {
-          params: { query: "Switzerland landscape cityscape" },
-          headers: {
-            Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`
+        const response = await axios.get(
+          "https://api.unsplash.com/photos/random",
+          {
+            params: { query: "Switzerland landscape cityscape" },
+            headers: {
+              Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`,
+            },
           }
-        });
+        );
         // Check if location data is available in the response
-        if (response.data && response.data.urls && response.data.urls.regular &&
-        response.data.location && response.data.location.position &&
-        response.data.location.position.latitude &&
-        response.data.location.position.longitude) {
-
+        if (
+          response.data &&
+          response.data.urls &&
+          response.data.urls.regular &&
+          response.data.location &&
+          response.data.location.position &&
+          response.data.location.position.latitude &&
+          response.data.location.position.longitude
+        ) {
           setImageUrl(response.data.urls.regular); // Set the image URL
 
           // Set the location state
           setLocation({
             lat: response.data.location.position.latitude,
-            lng: response.data.location.position.longitude
+            lng: response.data.location.position.longitude,
           });
           setTimer(10); // Reset and start the timer when a new image is fetched
         }
@@ -68,13 +74,21 @@ const GameRound = () => {
       <div className="gameround side-by-side-containers">
         <BaseContainer className="gameround container">
           {imageUrl && (
-              <img src={imageUrl} alt="Swiss Landscape" style={{ width: "100%", height: "auto", objectFit: "cover" }} />
+            <img
+              src={imageUrl}
+              alt="Swiss Landscape"
+              style={{ width: "100%", height: "auto", objectFit: "cover" }}
+            />
           )}
         </BaseContainer>
-        <BaseContainer title="Where was this image taken? Make your guess by clicking on the map!" className="gameround container" style={{ height: "600px" }}>
+        <BaseContainer
+          title="Where was this image taken? Make your guess by clicking on the map!"
+          className="gameround container"
+          style={{ height: "600px" }}
+        >
           <>
             <SwissMap />
-            <br/>
+            <br />
             <Timer initialCount={10} className="gameround title-font" />
           </>
         </BaseContainer>
