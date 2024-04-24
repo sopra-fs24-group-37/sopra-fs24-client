394eaf547ab772d9c5001c794eabd2f759754bce
Gian-Luca <gian-luca.fuehrer@uzh.ch>
2024-04-24 15:57:17 +0200

    feat: added a button

--- src/components/views/GameRound.tsx
+++ src/components/views/GameRound.tsx
@@ -10,20 +10,21 @@ import "leaflet/dist/leaflet.css";
 import Timer from "components/ui/Timer";
 import PropTypes from "prop-types";
 
-const GameRound = ({client}) => {
+const GameRound = ({ client }) => {
   const [players, setPlayers] = useState<User[]>([]);
   const navigate = useNavigate();
   const [imageUrl, setImageUrl] = useState("");
   const [location, setLocation] = useState({ lat: 46.8182, lng: 8.2275 });
   const [selectedLocation, setSelectedLocation] = useState(null);
   const [canInteract, setCanInteract] = useState(true); // State to control map interaction
-  const gameId = sessionStorage.getItem("gameId")
+  const gameId = sessionStorage.getItem("gameId");
+  const [timerExpired, setTimerExpired] = useState(false);
 
   useEffect(() => {
     const fetchImage = async (message) => {
       try {
         const response = await axios.get(
-          "https://api.unsplash.com/photos/"+message,
+          "https://api.unsplash.com/photos/" + message,
           {
             headers: {
               Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`,
@@ -49,11 +50,17 @@ const GameRound = ({client}) => {
         console.error("Failed to fetch image from Unsplash:", error);
       }
     };
-    const roundSubscription = client.subscribe("/topic/games/" + gameId + "/round" , message =>{
-      console.log(`Received: ${message.body}`);
-      fetchImage(message.body);
+    const roundSubscription = client.subscribe(
+      "/topic/games/" + gameId + "/round",
+      (message) => {
+        console.log(`Received: ${message.body}`);
+        fetchImage(message.body);
+      }
+    );
+    client.publish({
+      destination: "/app/games/" + gameId + "/round",
+      body: gameId,
     });
-    client.publish({ destination: "/app/games/" + gameId + "/round", body: gameId });
   }, []);
 
   const fetchPlayers = async () => {
@@ -76,13 +83,21 @@ const GameRound = ({client}) => {
   };
 
   const handleTimeUp = () => {
-    setCanInteract(false);  // This will disable map interaction when the timer expires
+    setCanInteract(false); // This will disable map interaction when the timer expires
+    setTimerExpired(true);
+  };
+
+  const doStuff = () => {
+    console.log("stuff is being done");
   };
 
   return (
     <div className="flex-center-wrapper">
       <div className="gameround side-by-side-containers">
-        <BaseContainer className="gameround container" style={{ height: "650px" }}>
+        <BaseContainer
+          className="gameround container"
+          style={{ height: "650px" }}
+        >
           {imageUrl && <img src={imageUrl} alt="Swiss Landscape" />}
         </BaseContainer>
         <BaseContainer
@@ -94,11 +109,16 @@ const GameRound = ({client}) => {
             <SwissMap
               onMapClick={handleMapClick}
               selectedLocation={selectedLocation}
-              imageLocation={!canInteract ? location : undefined}  // Pass the image location when the interaction is disabled
+              imageLocation={!canInteract ? location : undefined} // Pass the image location when the interaction is disabled
             />
             <br />
-            <Timer initialCount={10} onTimeUp={handleTimeUp} className="gameround title-font" />
+            <Timer
+              initialCount={10}
+              onTimeUp={handleTimeUp}
+              className="gameround title-font"
+            />
           </>
+          {timerExpired && <button onClick={() => doStuff()}>Button</button>}
         </BaseContainer>
       </div>
     </div>
@@ -106,7 +126,7 @@ const GameRound = ({client}) => {
 };
 
 GameRound.propTypes = {
-  client: PropTypes.object.isRequired,// Validate prop type
+  client: PropTypes.object.isRequired, // Validate prop type
 };
 
 export default GameRound;
