c0ba4e8a8bcd49cdc33cd79c0186b5a48a782e65
Skyl3ss <andri-spescha@swissonline.ch>
2024-04-25 21:30:23 +0200

    fix: adjusted to websocket url for secure websocket communication

--- src/helpers/getDomain.js
+++ src/helpers/getDomain.js
@@ -14,7 +14,7 @@ export const getDomain = () => {
 }
 
 export const getWSDomain = () => {
-  const prodUrl = "ws://sopra-fs24-group-37-server.oa.r.appspot.com/ws" // TODO: insert your prod url for server (once deployed)
+  const prodUrl = "wss://sopra-fs24-group-37-server.oa.r.appspot.com/ws" // TODO: insert your prod url for server (once deployed)
   const devUrl = "ws://localhost:8080/ws"
 
   return isProduction() ? prodUrl : devUrl
