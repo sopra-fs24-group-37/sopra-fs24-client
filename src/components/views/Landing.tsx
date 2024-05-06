209a742c65dac477cae46bc423d6164d53a02b04
manuel.widmer <manuel-widmer@bluewin.ch>
2024-05-06 14:03:51 +0200

    added intro sound to landing view

--- src/components/views/Landing.tsx
+++ src/components/views/Landing.tsx
@@ -1,18 +1,30 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Button } from "components/ui/Button";
 import "styles/views/Landing.scss";
 import BaseContainer from "components/ui/BaseContainer";
+import { Howl } from "howler";
+import IntroSound from "../../sounds/Intro.mp3"; // Import the sound file
 
 const Landing = () => {
-
   const navigate = useNavigate();
 
+  const playSound = () => {
+    const sound = new Howl({
+      src: [IntroSound],
+      autoplay: true,
+      loop: false,
+      volume: 1.0,
+    });
+  };
+
   const goToLogin = () => {
+    playSound(); // Play sound when the user clicks on the login button
     navigate("/login");
   };
 
   const goToRegistration = () => {
+    playSound(); // Play sound when the user clicks on the register button
     navigate("/registration");
   };
 
@@ -22,22 +34,13 @@ const Landing = () => {
         <div className="landing field">
           <div className="landing container">
             <div className="landing form">
-              <div className="landing title">
-                Welcome to Swissquiz!
-              </div>
+              <div className="landing title">Welcome to Swissquiz!</div>
               <div className="landing button-container">
-
-                <Button
-                  width="100%" // controls size of login button
-                  onClick={goToLogin}
-                >
-                Login
+                <Button width="100%" onClick={goToLogin}>
+                  Login
                 </Button>
-                <Button
-                  width="100%" // controls size of register button
-                  onClick={goToRegistration}
-                >
-              Register
+                <Button width="100%" onClick={goToRegistration}>
+                  Register
                 </Button>
               </div>
             </div>
